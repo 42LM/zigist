2c12a5def27cd1e4af0d25f0f88ea82d4da1504d
Lukas Moeller <93522910+lmllrjr@users.noreply.github.com>
2023-12-09 09:55:25 +0100

    update zig version to `0.12.0-dev.1664+8ca4a5240`
    Fixing:
    - note: consider using 'const'
    - error: local variable is never mutated

--- src/main.zig
+++ src/main.zig
@@ -79,7 +79,7 @@ pub fn main() !void {
     const dateTime = timestamp2DateTime(time.timestamp());
     const timestamp = try dateTime2String(alloc, dateTime);
 
-    var payload = std.fmt.allocPrint(
+    const payload = std.fmt.allocPrint(
         alloc,
         \\ {{
         \\ "files":{{
@@ -129,7 +129,7 @@ fn Conv(alloc: Allocator, s: []u8, punchline: bool) ![]u8 {
 }
 
 fn Getenv(name: []const u8) error{MissingEnvironmentVariable}![]const u8 {
-    var env = std.os.getenv(name);
+    const env = std.os.getenv(name);
 
     if (env == null) {
         return ZigistError.MissingEnvironmentVariable;
@@ -248,7 +248,7 @@ pub fn timestamp2DateTime(timestamp: i64) DateTime {
     const TAGN_AD_1970_01_01 = 719468; //* Tagnummer bezogen auf den 1. Maerz des Jahres "Null" */
 
     var tagN: u64 = TAGN_AD_1970_01_01 + unixtime / SEKUNDEN_PRO_TAG;
-    var sekunden_seit_Mitternacht: u64 = unixtime % SEKUNDEN_PRO_TAG;
+    const sekunden_seit_Mitternacht: u64 = unixtime % SEKUNDEN_PRO_TAG;
     var temp: u64 = 0;
 
     // Schaltjahrregel des Gregorianischen Kalenders:
@@ -265,8 +265,8 @@ pub fn timestamp2DateTime(timestamp: i64) DateTime {
 
     // TagN enthaelt jetzt nur noch die Tage des errechneten Jahres bezogen auf den 1. Maerz.
     var monat: u8 = @intCast((5 * tagN + 2) / 153);
-    var monatu64: u64 = @intCast(monat);
-    var tag: u8 = @intCast(tagN - (monatu64 * 153 + 2) / 5 + 1);
+    const monatu64: u64 = @intCast(monat);
+    const tag: u8 = @intCast(tagN - (monatu64 * 153 + 2) / 5 + 1);
     //  153 = 31+30+31+30+31 Tage fuer die 5 Monate von Maerz bis Juli
     //  153 = 31+30+31+30+31 Tage fuer die 5 Monate von August bis Dezember
     //        31+28          Tage fuer Januar und Februar (siehe unten)
@@ -279,15 +279,15 @@ pub fn timestamp2DateTime(timestamp: i64) DateTime {
         jahr += 1;
     }
 
-    var stunde: u8 = @intCast(sekunden_seit_Mitternacht / 3600);
-    var minute: u8 = @intCast(sekunden_seit_Mitternacht % 3600 / 60);
-    var sekunde: u8 = @intCast(sekunden_seit_Mitternacht % 60);
+    const stunde: u8 = @intCast(sekunden_seit_Mitternacht / 3600);
+    const minute: u8 = @intCast(sekunden_seit_Mitternacht % 3600 / 60);
+    const sekunde: u8 = @intCast(sekunden_seit_Mitternacht % 60);
 
     return DateTime{ .day = tag, .month = monat, .year = jahr, .hour = stunde, .minute = minute, .second = sekunde };
 }
 
 fn dateTime2String(alloc: Allocator, dt: DateTime) ![]u8 {
-    var ts = try std.fmt.allocPrint(alloc, "{d}.{:0<2}.{:0<4} {d}:{:0<2}:{:0<2}", .{ dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second });
+    const ts = try std.fmt.allocPrint(alloc, "{d}.{:0<2}.{:0<4} {d}:{:0<2}:{:0<2}", .{ dt.day, dt.month, dt.year, dt.hour, dt.minute, dt.second });
     // defer alloc.free(ts);
 
     return ts;
